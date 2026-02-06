6a685781135208ae3f6cbb9b862f50459bc5af69
kapil5655 <kapil77001@gmail.com>
2026-02-06 13:52:42 +0530

    Arrayreverse

---
+++ Arrayreverse.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class Arrayreverse{
+
+    public static void main(String[] args){
+
+        // by Two -pointer Time complexity _ O(n) space : O(1)
+
+        int[] arr={1,2,3,4,5};
+        // int i = 0; int j = arr.length-1;
+        // while(i<j){
+        //     int temp = arr[i];
+        //     arr[i] = arr[j];
+        //     arr[j] = temp;
+
+        //     i++;
+        //     j--;
+        // }
+        // for(int x : arr){
+        //     System.out.print(x+" ");
+        // }
+
+
+        //bY - stack time - O(n) space - O(n)
+        Stack<Integer>st =  new Stack<>();
+
+        for(int i = 0; i<=arr.length - 1; i++){
+            int element = arr[i];
+            st.push(element);
+        }
+        while(!st.isEmpty()){
+            int element = st.peek();
+            st.pop();
+            System.out.print(element+" ");
+
+        }
+
+
+    }
+}
+
