48f0899ca952da893efbd0dc2ca7e541b92e8993
kapil5655 <kapil77001@gmail.com>
2026-02-08 01:30:22 +0530

    FirstNonRepeating

---
+++ FirstNonRepeating.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class FirstNonRepeating {
+
+    public static void firstNonRepeating(int[] arr){
+      
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num, 0)+1);
+             
+        }
+            
+            for( int x : arr){
+                if(map.get(x) == 1){
+                    System.out.print(x);
+                    break;
+                }
+            }
+     
+       
+    }
+    public static void main(String[] args){
+        int[] arr = {9,5,3,2,9,6,5};
+         
+        firstNonRepeating(arr);
+    }
+}
