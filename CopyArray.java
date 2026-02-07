ee1cd5ea91b98e16169349eaca6c47e3cc5d2044
kapil5655 <kapil77001@gmail.com>
2026-02-08 00:44:18 +0530

    CopyArray

--- CopyArray.java
+++ CopyArray.java
@@ -1,8 +1,10 @@
+
+
 public class CopyArray {
     public static void main(String [] args){
     //    1. //for loop interview preffered
-        int [] arr1 = {1,2,3,4,5};
-        int[] arr2 = new int[arr1.length];
+        int [] arr1 = {1,2,3,4,5,35};
+        // int[] arr2 = new int[arr1.length];
         // for(int i = 0; i<arr1.length; i++){
         //     arr2[i] = arr1[i];
 
@@ -13,11 +15,21 @@ public class CopyArray {
      
     // 2. System.arrcopy() method
     //syntax - System.arraycopy(source,srcPos,dest,destPOs, length);
-    System.arraycopy(arr1,0,arr2,0,arr1.length);
+    // System.arraycopy(arr1,0,arr2,0,arr1.length);
 
-    for(int x : arr2){
-         System.out.print(x+" ");
-    }
+    // for(int x : arr2){
+    //      System.out.print(x+" ");
+    // }
+
+  // 3.clone method
+  int []arr2 = arr1.clone();
+  for(int x  : arr2){
+    System.out.print(x + " ");
+  }
+
+
+
+    
 
     }
 }
