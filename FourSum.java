986ecbf98cc936394e5da9ae238c000605d91862
kapil5655 <kapil77001@gmail.com>
2026-02-07 06:17:00 +0530

    FourSum

--- FourSum.java
+++ FourSum.java
@@ -1,17 +1,18 @@
 import java.util.Arrays;
 
-public class ThreeSum {
+public class FourSum {
     //using sorting+loop time - O(n*n) space - O(1);
     //this can also be done by hashset + loop But it also take time - O(n) 
     // so we prefer sorting because it is easy
-         public static boolean threeSum(int []arr,int target){
+         public static boolean fourSum(int []arr,int target){
             int n = arr.length;
             Arrays.sort(arr);
-              for(int i=0; i<n-2; i++){
-                    int left = 0; 
+              for(int i=0; i<n-3; i++){
+                  for(int j = i+1; j<n-2; j++){
+                      int left = j+1; 
                     int right = arr.length - 1;
                     while(left<right){
-                        int sum = arr[left] + arr[right] + arr[i];
+                        int sum = arr[left] + arr[right] + arr[i] + arr[j];
                         if(sum==target){
                             return true;
                         }
@@ -22,12 +23,14 @@ public class ThreeSum {
                             right--;
                         }
                     }   
+                  }
               }
               return false;
          }
     public static void main(String[] args) {
-        int[] arr = {1,2,-1,0,-2,1};
-        int target = 5;
-        System.out.println(threeSum(arr,target));
+        int[] arr = {2,2,2,2};
+        int target = 8;
+        System.out.println(fourSum(arr,target));
     }
 }
+
