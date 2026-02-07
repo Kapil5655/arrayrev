3b47a014c3ae91ce8373d5cb958f2ab2e2f4c685
kapil5655 <kapil77001@gmail.com>
2026-02-08 01:34:46 +0530

    FirstRepeating

---
+++ FirstRepeating.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class FirstRepeating {
+     public static void firstRepeating(int[] arr){
+      
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num, 0)+1);
+             
+        }
+            
+            for( int x : arr){
+                if(map.get(x) > 1){
+                    System.out.print(x);
+                    break;
+                }
+            }
+     
+       
+    }
+    public static void main(String[] args){
+        int[] arr = {6,5,3,5,9,6,5};
+         
+        firstRepeating(arr);
+    }
+}
